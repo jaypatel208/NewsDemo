27b3651b69cad04b8765dd076b4a1565539dbf31
Jay Patel <jay037493@gmail.com>
2023-11-01 19:34:01 +0530

    Material implementation in components

--- app/src/main/java/com/example/newsdemo/ui/theme/components/AppComponents.kt
+++ app/src/main/java/com/example/newsdemo/ui/theme/components/AppComponents.kt
@@ -12,16 +12,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -30,7 +32,6 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.newsdemo.R
 import com.example.newsdemo.data.entity.Article
-import com.example.newsdemo.ui.theme.Purple40
 
 @Composable
 fun Loader() {
@@ -44,33 +45,67 @@ fun Loader() {
         CircularProgressIndicator(
             modifier = Modifier
                 .size(60.dp)
-                .padding(10.dp), color = Purple40
+                .padding(10.dp), color = MaterialTheme.colorScheme.primary
         )
     }
 }
 
 @Composable
-fun NormalTextComponent(textValue: String) {
+fun NormalTextComponent(textValue: String, centerAligned: Boolean = false) {
     Text(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
             .wrapContentHeight(), text = textValue, style = TextStyle(
-            fontSize = 18.sp, fontWeight = FontWeight.Normal, fontFamily = FontFamily.Monospace, color = Purple40
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Normal,
+            fontFamily = FontFamily(Font(R.font.roboto_regular, FontWeight.Medium)),
+            color = MaterialTheme.colorScheme.onSurface,
+            textAlign = if (centerAligned) TextAlign.Center else TextAlign.Start
         )
     )
 }
 
 @Composable
-fun HeadingTextComponent(textValue: String, centerAligned: Boolean = false) {
+fun HeadingTextComponent(textValue: String) {
     Text(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
             .wrapContentHeight(),
         text = textValue,
-        style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Medium),
-        textAlign = if (centerAligned) TextAlign.Center else TextAlign.Start
+        style = TextStyle(
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Medium,
+            fontFamily = FontFamily(Font(R.font.roboto_regular, FontWeight.Normal)),
+            color = MaterialTheme.colorScheme.primary
+        )
+    )
+}
+
+@Composable
+fun AuthorTextComponent(textValue: String) {
+    Text(
+        modifier = Modifier
+            .wrapContentSize(), text = textValue, style = TextStyle(
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Normal,
+            fontFamily = FontFamily(Font(R.font.roboto_condensed, FontWeight.Medium)),
+            color = MaterialTheme.colorScheme.inverseSurface, letterSpacing = 0.5.sp
+        )
+    )
+}
+
+@Composable
+fun SourceTextComponent(textValue: String) {
+    Text(
+        modifier = Modifier
+            .wrapContentSize(), text = textValue, style = TextStyle(
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Normal,
+            fontFamily = FontFamily(Font(R.font.roboto_condensed, FontWeight.Medium)),
+            color = MaterialTheme.colorScheme.inverseSurface, letterSpacing = 0.5.sp
+        )
     )
 }
 
@@ -82,13 +117,13 @@ fun AuthorDetailsComponent(authorName: String?, sourceName: String?) {
             .padding(start = 10.dp, end = 10.dp, bottom = 24.dp)
     ) {
         authorName?.also {
-            Text(text = it)
+            AuthorTextComponent(textValue = it)
         }
 
         Spacer(modifier = Modifier.weight(1f))
 
         sourceName?.also {
-            Text(text = it)
+            SourceTextComponent(textValue = it)
         }
     }
 }
@@ -106,7 +141,7 @@ fun EmptyStateComponent() {
             painter = painterResource(id = R.drawable.news_no_data), contentDescription = null
         )
 
-        HeadingTextComponent(textValue = stringResource(R.string.no_news), centerAligned = true)
+        NormalTextComponent(textValue = stringResource(R.string.no_news), centerAligned = true)
     }
 }
 
@@ -123,7 +158,7 @@ fun NoNetworkComponent() {
             painter = painterResource(id = R.drawable.no_network), contentDescription = null
         )
 
-        HeadingTextComponent(textValue = stringResource(R.string.no_internet), centerAligned = true)
+        NormalTextComponent(textValue = stringResource(R.string.no_internet), centerAligned = true)
     }
 }
 
@@ -133,7 +168,7 @@ fun NewsRowComponent(page: Int, article: Article) {
         modifier = Modifier
             .fillMaxSize()
             .padding()
-            .background(Color.White)
+            .background(MaterialTheme.colorScheme.surface)
     ) {
         AsyncImage(
             modifier = Modifier
